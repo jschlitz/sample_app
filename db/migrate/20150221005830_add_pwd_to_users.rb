de08fdfa0c057c2ec34b13d18e82b3b714883650
Jacob Schlitz <jschlitz@gmail.com>
2015-02-20 20:09:44 -0500

    added some validations to user.

---
+++ db/migrate/20150221005830_add_pwd_to_users.rb
@@ -0,0 +1,5 @@
+class AddPwdToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,39 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def  setup
+    @user =  User.new(name: "Example User", email: "user@example.com",
+      password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "Name should be" do
+    @user.name = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be" do
+    @user.email = "    "
+    assert @user.invalid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = duplicate_user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
