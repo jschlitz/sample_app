306e86a4ae55da4f8f457f79afb73fd9fa0a9ddc
Jacob Schlitz <jschlitz@gmail.com>
2015-03-12 18:32:19 -0400

    delete stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,4 +36,8 @@ class User < ActiveRecord::Base
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 end
